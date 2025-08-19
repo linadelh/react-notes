3149d17f2982db1c8ad0e3587f0091788ba5d279
linadelh <lina35979@gmail.com>
2025-08-19 22:41:50 +0200

    saha

--- src/Mybutton.js
+++ src/Mybutton.js
@@ -5,15 +5,23 @@ import { useState } from "react";
 
 export default function MyButton (){
 
-  const [ value , setvalue ] = useState("welcome,  click here if you want to change this sentence");
+  const [ value , setvalue ] = useState("welcome");
+  console.log(value);
   return (
     <div>
         <h1>{value}</h1>
         <button onClick={()=>{
+            if(value === "welcome"){
             setvalue("changed weeee"); 
+            console.log(useState[0]);
+            }else{
+                setvalue("welcome"); 
+            }
         }}>click here</button>
-    </div>
 
+          
+    </div>
+      
   )
   
 }
