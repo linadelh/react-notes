bda7c4d5f953c70d0e5c280b6126b4d4c953b5c9
linadelh <lina35979@gmail.com>
2025-08-23 16:24:03 +0200

    splice

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 
+import { useState } from 'react';
 import './App.css';
-import Mybutton from './Mybutton.js'
-import MyInput from './MyInput.js'
-import MyForm from './MyForm.js';
 
+
+   let nextid = 5 ; 
 function App() {
 
-  
+  function handleclick(){
+     setfruits([...fruits ,{ id : nextid , name : fruitName } ]);
+     nextid = nextid + 1 ; 
+  }
+
+   function handledeleteclick(id) {
+    const newFruits = [...fruits];
+   let index = 0 ;
+   for( let fruit of newFruits){
+    if(fruit.id === id){
+      newFruits.splice(index , 1);
+      setfruits(newFruits);
+    }else{
+      index++; 
+    }
+   }
+}
+
+  const [fruitName , setFruitName] = useState(""); 
+  const [fruits , setfruits ] = useState(
+    [    
+      {id: 1 , name : "phone"} ,
+      {id: 2 , name : "phone1"},
+      {id: 3 , name : "phone2"},
+      {id: 4 , name : "phone3"}
+    
+    ]
+  );
+  const fruitstable = fruits.map((fruit)=>{
+    return (
+      <li key={fruit.id}>{fruit.name}<button onClick={()=>{
+        handledeleteclick(fruit.id)
+      }
+      }>Delete</button></li>
+    )
+  })
 
   return ( 
-    <div className="App"  style={{padding:"150px"}}>
- 
-
-     <MyForm/>
-
-
-
-
-
-
-
-
-
-
-
+    <div className="App"  style={{padding:"150px" , fontSize :"40px"}}>
+          {fruitstable}
+          <input type='text' value={fruitName} onChange={(e)=>{
+                  setFruitName(e.target.value);
+          }}/>
+          <button onClick={handleclick}>add</button>
     </div>
   );
 }
-
-
-
-
-
 export default App;
