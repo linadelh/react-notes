32bf535f87d0ae3daa38839bcc800bff7041c18d
linadelh <lina35979@gmail.com>
2025-09-12 18:07:26 +0200

    calculatrice de lina

--- Calculatrice-De-Lina/src/Calculatrice.js
+++ Calculatrice-De-Lina/src/Calculatrice.js
@@ -2,100 +2,98 @@ import { useState } from "react"
 
 
 export default function Calculatrice (){
+      const [currentNumber , setcurrentNumber] = useState("");
+      const [prevNumber , setPrevNumber] = useState(""); 
+      const [operation , setOperation] = useState("");
 
 
-   
-   const [firstNumber , setFirstnumber] = useState(0);
-   const [SecondNumber , setSecondNumber] = useState(0);
-   const [operation , setOperation] = useState(""); 
-   const [Total , setTotal] = useState(0); 
-   const [currentInput , setcurrentInput] = useState(0);
-    
 
-    function HandleClick(e){
 
+
+    function HandleClick(number){
+    if(operation === ""){
+        setcurrentNumber(currentNumber.toString() + number); 
+    }else{
+        if(prevNumber ===""){
+            setPrevNumber(currentNumber);
+            setcurrentNumber("" + number) ;
+        }else{
+        setcurrentNumber(currentNumber.toString() + number );
+        }
+    }
+    }
+
+
+
+    function HandleOperationClick(op){
         let total ; 
+       
 
-       if(operation==="" && !(["+" , "/" , "*" , "=" , "-"].includes(e.target.value))){
-        if(firstNumber !== 0 ){
-        setFirstnumber(firstNumber.toString() + e.target.value);
-        setcurrentInput(firstNumber.toString() + e.target.value);
-        }else{
-            setFirstnumber(e.target.value);
+        if(op === "="){
+            if(operation === "+"){
+                total = parseFloat(prevNumber) + parseFloat(currentNumber) ;
+            }
+        
+            if(operation ==="-"){
+                total = parseFloat(prevNumber) - parseFloat(currentNumber)  ; 
+            }
+
+            
+            if(operation ==="/"){
+                total = parseFloat(prevNumber) / parseFloat(currentNumber)  ; 
+            }
+
+            
+            if(operation ==="*"){
+                total = parseFloat(prevNumber) * parseFloat(currentNumber)  ; 
+            }
+
+        setcurrentNumber(total.toString());
+        }else if(op === "+"){
+            setOperation("+");
+        }else if(op === "-"){
+            setOperation("-");
+        }else if(op === "*"){
+             setOperation("*");
+        }else if(op === "/"){
+           setOperation("/")
         }
+    }
 
-       } else if(["+" , "/" , "*" , "=" , "-"].includes(e.target.value)){
-        setOperation(e.target.value);
-          
-        if(e.target.value === "="){
 
-        if(operation === "-"){
-             total = parseFloat(firstNumber) - parseFloat(SecondNumber);
-
-        } else if(operation === "/"){
-            total = parseFloat(firstNumber) / parseFloat(SecondNumber);
-        }else if(operation === "+"){
-            total = parseFloat(firstNumber) + parseFloat(SecondNumber);
-
-        }else if(operation === "*"){
-            total = parseFloat(firstNumber) * parseFloat(SecondNumber);
-        }
-            setTotal(total); 
-            setcurrentInput(total);
-             setFirstnumber(total);
-            setSecondNumber(0);
-              setOperation("");
-           
-        }
-       }else{
-         if(SecondNumber !== 0 ){
-        setSecondNumber(SecondNumber.toString() + e.target.value);
-        setcurrentInput(SecondNumber.toString() + e.target.value);
-        }else{
-            setSecondNumber(e.target.value);
-        }
-       }
-
+    function handleResetButton(){
+        setcurrentNumber("0");
+        setPrevNumber("");
+        setOperation("");
     }
    
 
-    function handleclickreset (){
-        setcurrentInput(0);
-        setFirstnumber(0);
-        setSecondNumber(0);
-        setOperation("");
-        setTotal(0);
-    }
+   
     return (
         <>
          <h1>CALCULATRICE DE LINA</h1>
-
          <div className="calculatrice">
-
             <div className="Inputs">
-                <button onClick={handleclickreset}>Reset</button>
-            <input type="text" disabled={true} value = {currentInput} ></input>
+                <button>Reset</button>
+            <input type="text" disabled={true} value={currentNumber}></input>
             </div>
-
-              
-
             <div className="oprations">
-                <button value={7} onClick={HandleClick}>7</button>
-                <button value={8} onClick={HandleClick} >8</button>
-                <button value={9} onClick={HandleClick} >9</button>
-                <button value={"/"} onClick={HandleClick} >/</button>
-                <button value={4} onClick={HandleClick} >4</button>
-                <button value={5} onClick={HandleClick} >5</button>
-                <button value={6} onClick={HandleClick}>6</button>
-                <button value={"-"} onClick={HandleClick} >-</button>
-                <button value={1} onClick={HandleClick} >1</button>
-                <button value={2} onClick={HandleClick} >2</button>
-                <button value={3}onClick={HandleClick} >3</button>
-                <button value={"+"}onClick={HandleClick} >+</button>
-                <button value={0}onClick={HandleClick} >0</button>
-                <button value={"."} onClick={HandleClick} >.</button>
-                <button value={"="}onClick={HandleClick} >=</button>
-                <button value={"*"}onClick={HandleClick} >*</button>
+                <button onClick={()=>{HandleClick(7)}} >7</button>
+                <button onClick={()=>{HandleClick(8)}} >8</button>
+                <button onClick={()=>{HandleClick(9)}} >9</button>
+                <button onClick={()=>{HandleOperationClick("/")}} >/</button>
+                <button onClick={()=>{HandleClick(4)}} >4</button>
+                <button onClick={()=>{HandleClick(5)}} >5</button>
+                <button onClick={()=>{HandleClick(6)}} >6</button>
+                <button onClick={()=>{HandleOperationClick("-")}}>-</button>
+                <button onClick={()=>{HandleClick(1)}} >1</button>
+                <button onClick={()=>{HandleClick(2)}} >2</button>
+                <button onClick={()=>{HandleClick(3)}} >3</button>
+                <button onClick={()=>{HandleOperationClick("+")}} >+</button>
+                <button onClick={()=>{HandleClick(0)}}>0</button>
+                <button >.</button>
+                <button  onClick={()=>{HandleOperationClick("=")}} >=</button>
+                <button  onClick={()=>{HandleOperationClick("*")}} >*</button>
             </div>
 
          </div>
